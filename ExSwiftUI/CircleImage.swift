1bfdab465595cacb10ab2845d1eba10e25c05b26
makoto.ogawa <makoto.ogawa@smartdrive.co.jp>
2020-02-07 09:02:11 +0900

    first commit

---
+++ ExSwiftUI/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  ExSwiftUI
+//
+//  Created by ogawa on 2020/02/07.
+//  Copyright © 2020 ogawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# ExSwiftUI
+
+## docs
+
+https://developer.apple.com/tutorials/swiftui/creating-and-combining-views
